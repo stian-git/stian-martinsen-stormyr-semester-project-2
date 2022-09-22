2ea028237e899934c172c06ca89408bf07f8802e
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-22 17:09:44 +0200

    SP2-129

--- js/components/ui/addFooter.js
+++ js/components/ui/addFooter.js
@@ -2,14 +2,16 @@ import { footerContainer } from "../variables.js";
 
 export default function addFooter() {
     footerContainer.innerHTML = `
-    <div class="footer_some">
-        <i class="fa-brands fa-square-facebook"></i>
-        <i class="fa-brands fa-square-instagram"></i>
-        <i class="fa-brands fa-square-snapchat"></i>
+    <div class="footer__section">
+        <i class="fa-brands fa-square-facebook footer__section-someicon" ></i>
+        <i class="fa-brands fa-square-instagram footer__section-someicon"></i>
+        <i class="fa-brands fa-square-snapchat footer__section-someicon"></i>
     </div>
-    <div>Org.nr: 987 654 321</div>
-    <div>
-        <p>&copy; 2022</p>
-        <p>Stian Martinsen-Stormyr</p>
+    <div class="footer_section">
+        <p class="footer__section-info">Org.nr: 987 654 321</p>
+    </div>
+    <div class="footer_section">
+        <p class="footer__section-info">&copy; 2022</p>
+        <p class="footer__section-info">Stian Martinsen-Stormyr</p>
     </div>`;
 }
