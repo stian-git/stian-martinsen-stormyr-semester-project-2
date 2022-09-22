24ec5dafe7a68fc33942f9c3bcf053fabaddf008
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-22 14:30:28 +0200

    Some adjustments

--- js/components/ui/toggleSortingOfEditableProducts.js
+++ js/components/ui/toggleSortingOfEditableProducts.js
@@ -2,20 +2,17 @@ import filterEditableProducts from "../actions/filterEditableProducts.js";
 
 export default function toggleSortingOfEditableProducts() {
     const isActive = this.classList.contains("sorting-enabled");
-    console.log(this);
     if (isActive) {
         //toggle ascending or descending.
         if (this.classList.contains("ascending")) {
             this.classList.add("descending");
             this.classList.remove("ascending");
-            this.firstElementChild.lastElementChild.innerHTML = "Down";
         } else {
             this.classList.remove("descending");
             this.classList.add("ascending");
-            this.firstElementChild.lastElementChild.innerHTML = "Up";
         }
     } else {
-        // when column was not already the sort field we remove the previous one and use this instead
+        // when column was not already the active sort field we remove the previous one and use this instead
         document.querySelector("th.sorting-enabled").classList.remove("sorting-enabled");
         this.classList.add("sorting-enabled");
     }
