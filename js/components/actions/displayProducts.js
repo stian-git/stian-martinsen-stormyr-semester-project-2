75665d15cfe8cb2a1d8739d5a64c529e6790d7b0
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-23 21:52:01 +0200

    Some styling of products

--- js/components/actions/displayProducts.js
+++ js/components/actions/displayProducts.js
@@ -18,13 +18,11 @@ export default function displayProducts(arr) {
                         <div class="product__header_imagecontainer_buymebanner productcard__box-imagewrapper-banner" data-id="${product.id}" data-title="${product.attributes.title}" data-model="${product.attributes.productnumber}" data-img="${product.attributes.image_url}" data-price="${product.attributes.price}" >Add to cart.</div>
                     </div>
                     <div class="card-body productcard__box-body">
-                      <h6 class="card-title">${product.attributes.title}</h6>
-                      <!-- <p class="card-text"> -->
+                        <h6 class="card-title productcard__box-body-title">${product.attributes.title}</h6>
                         <div class="product__details-row card-text productcard__box-details" >
                             <p class="product__details_price productcard__box-details-price">${product.attributes.price}<span class="currency productcard__box-details-price-currency"> ${currency}</span></p>
                             <p class="product__details_stock productcard__box-details-stock">Stock: ${product.attributes.stock}</p>
                         </div>
-                    <!-- </p> -->
                     </div>
                     <div class="card-footer productcard__box-footer" data-prodid="${product.id}">
                         <small class="text-muted editproductbutton">Edit</small>
