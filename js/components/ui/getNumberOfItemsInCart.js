5cea568a87eb2a4af4cfdaa53381b4d718fc5528
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-23 21:24:11 +0200

    SP2-141 - CurrentCartItems failed on Firefox

--- js/components/ui/getNumberOfItemsInCart.js
+++ js/components/ui/getNumberOfItemsInCart.js
@@ -2,13 +2,15 @@ import { cartNavItem, storeCartContent } from "../variables.js";
 
 export default function getNumberOfItemsInCart() {
     const currentCartItems = JSON.parse(localStorage.getItem(storeCartContent));
-    let numberOfItems = 0;
-    currentCartItems.forEach((item) => {
-        numberOfItems += item.qty;
-    });
-    if (numberOfItems === 0) {
-        cartNavItem.innerHTML = "Cart";
-    } else {
-        cartNavItem.innerHTML = `Cart (${numberOfItems})`;
+    if (currentCartItems) {
+        let numberOfItems = 0;
+        currentCartItems.forEach((item) => {
+            numberOfItems += item.qty;
+        });
+        if (numberOfItems === 0) {
+            cartNavItem.innerHTML = "Cart";
+        } else {
+            cartNavItem.innerHTML = `Cart (${numberOfItems})`;
+        }
     }
 }
