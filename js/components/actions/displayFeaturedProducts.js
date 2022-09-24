a8116659eebf0307e335548ed37cbd0e6bbfa36e
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-24 21:53:50 +0200

    Test of troubleshoting solution

--- js/components/actions/displayFeaturedProducts.js
+++ js/components/actions/displayFeaturedProducts.js
@@ -9,75 +9,78 @@ export default function displayFeaturedProducts(arr) {
     const carouselContainer = document.querySelector("#featuredproducts .carousel-inner");
     //empty current carousel:
     carouselContainer.innerHTML = "";
-    const cardsInRow = 1 + Math.ceil((carouselContainer.offsetWidth - moreColsOnOffsetWidth) / colWidth);
+    const cardsInRow = 1 + Math.ceil(carouselContainer.offsetWidth - moreColsOnOffsetWidth / colWidth);
     const rowsToBuild = Math.ceil(arr.length / cardsInRow);
     // Build the carousel "rows":
-    for (let i = 0; i <= rowsToBuild; i++) {
-        console.log("Building row: " + i);
-        const rowClass = "carousel-item-row" + i;
+    for (let j = 0; j < rowsToBuild; j++) {
+        console.log("Building row: " + j);
+        const rowClass = "carousel-item-row" + j;
         console.log(rowClass);
         carouselContainer.innerHTML += `
-        <div class="featured__container carousel-item" data-bs-interval="40000">
+        <div class="featured__container carousel-item ${rowClass}" data-bs-interval="40000">
             <div class="featured__containerrow row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-2"></div>
         </div>`;
     }
-    // // Then add each featured product in the correct row.
-    // for (let i = 0; i < arr.length; i++) {
-    //     // Calculates the correct row:
-    //     const rowToGoTo = Math.floor(i / cardsInRow);
-    //     // Use the rownumber to identify the row-element to put it into
-    //     const carouselRowElement = document.querySelector("#featuredproducts .carousel-item-row" + rowToGoTo + " .row");
-    //     carouselRowElement.innerHTML += `
-    //             <div class="col featured__wrapper" data-id="${arr[i].id}">
-    //                 <div class="card h-100 featured__wrapper-card">
-    //                     <div class="card-img featured__wrapper-card-imgbox">
-    //                         <img src="${arr[i].attributes.image_url}" class="card-img-top featured__wrapper-card-imgbox-img" aria-label="Image of ${arr[i].attributes.title}" title="Image of ${arr[i].attributes.title}" alt="Image of ${arr[i].attributes.title}" />
-    //                     </div>
-    //                     <div class="card-body">
-    //                         <h5 class="card-title">${arr[i].attributes.title}</h5>
-    //                         <p class="card-text">${arr[i].attributes.price} EUR</p>
-    //                     </div>
-    //                     <div class="card-footer">Model: ${arr[i].attributes.productnumber}</div>
-    //                 </div>
-    //             </div>
-    //     `;
-    // }
 
-    // // set first carousel-item as the active.
-    // document.querySelector("#featuredproducts .carousel-item").classList.add("active");
+    // Above is the problem....
 
-    // // add eventlisteners:
+    // Then add each featured product in the correct row.
+    for (let i = 0; i < arr.length; i++) {
+        // Calculates the correct row:
+        const rowToGoTo = Math.floor(i / cardsInRow);
+        // Use the rownumber to identify the row-element to put it into
+        const carouselRowElement = document.querySelector("#featuredproducts .carousel-item-row" + rowToGoTo + " .row");
+        carouselRowElement.innerHTML += `
+                <div class="col featured__wrapper" data-id="${arr[i].id}">
+                    <div class="card h-100 featured__wrapper-card">
+                        <div class="card-img featured__wrapper-card-imgbox">
+                            <img src="${arr[i].attributes.image_url}" class="card-img-top featured__wrapper-card-imgbox-img" aria-label="Image of ${arr[i].attributes.title}" title="Image of ${arr[i].attributes.title}" alt="Image of ${arr[i].attributes.title}" />
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">${arr[i].attributes.title}</h5>
+                            <p class="card-text">${arr[i].attributes.price} EUR</p>
+                        </div>
+                        <div class="card-footer">Model: ${arr[i].attributes.productnumber}</div>
+                    </div>
+                </div>
+        `;
+    }
 
-    // const allFeaturedProducts = document.querySelectorAll("#featuredproducts .col");
-    // allFeaturedProducts.forEach((element) => {
-    //     element.addEventListener("click", (e) => {
-    //         // We catches the ID from any of the clickable elements:
-    //         let id;
-    //         switch (e.path.length) {
-    //             case 14:
-    //                 id = e.target.parentElement.parentElement.parentElement.dataset.id;
-    //                 break;
-    //             case 13:
-    //                 id = e.target.parentElement.parentElement.dataset.id;
-    //                 break;
-    //             case 12:
-    //                 id = e.target.parentElement.dataset.id;
-    //                 break;
-    //             case 11:
-    //                 id = e.target.dataset.id;
-    //                 break;
-    //             default:
-    //                 id = "";
-    //                 break;
-    //         }
+    // set first carousel-item as the active.
+    document.querySelector("#featuredproducts .carousel-item").classList.add("active");
 
-    //         // only forward the user to products page if id is found.
-    //         if (!isNaN(parseInt(id))) {
-    //             //console.log("ID is a number and we will forward: " + id);
-    //             window.location.href = "/productdetails.html?id=" + id;
-    //         }
-    //     });
-    // });
+    // add eventlisteners:
+
+    const allFeaturedProducts = document.querySelectorAll("#featuredproducts .col");
+    allFeaturedProducts.forEach((element) => {
+        element.addEventListener("click", (e) => {
+            // We catches the ID from any of the clickable elements:
+            let id;
+            switch (e.path.length) {
+                case 14:
+                    id = e.target.parentElement.parentElement.parentElement.dataset.id;
+                    break;
+                case 13:
+                    id = e.target.parentElement.parentElement.dataset.id;
+                    break;
+                case 12:
+                    id = e.target.parentElement.dataset.id;
+                    break;
+                case 11:
+                    id = e.target.dataset.id;
+                    break;
+                default:
+                    id = "";
+                    break;
+            }
+
+            // only forward the user to products page if id is found.
+            if (!isNaN(parseInt(id))) {
+                //console.log("ID is a number and we will forward: " + id);
+                window.location.href = "/productdetails.html?id=" + id;
+            }
+        });
+    });
 }
 
 // < 576 = 1 // offset =< 328                   0
