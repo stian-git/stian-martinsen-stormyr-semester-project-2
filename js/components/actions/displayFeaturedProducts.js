1d23fbab93eefa574a39dfc63cdc2a48f910b25a
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-24 21:31:28 +0200

    Partial test of function

--- js/components/actions/displayFeaturedProducts.js
+++ js/components/actions/displayFeaturedProducts.js
@@ -2,48 +2,48 @@
 // carousel-item
 export default function displayFeaturedProducts(arr) {
     console.log("Running displayFeaturedProducts...");
-    // variables used to calculate number of rows.
-    // const moreColsOnOffsetWidth = 328;
-    // const colWidth = 153;
-    // const carouselContainer = document.querySelector("#featuredproducts .carousel-inner");
-    // //empty current carousel:
-    // carouselContainer.innerHTML = "";
-    // const cardsInRow = 1 + Math.ceil((carouselContainer.offsetWidth - moreColsOnOffsetWidth) / colWidth);
-    // const rowsToBuild = Math.ceil(arr.length / cardsInRow);
-    // // Build the carousel "rows":
-    // for (let j = 0; j < rowsToBuild; j++) {
-    //     //console.log("Building row: " + j);
-    //     carouselContainer.innerHTML += `
-    //     <div class="featured__container carousel-item carousel-item-row${j}" data-bs-interval="40000">
-    //         <div class="featured__containerrow row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-2">
-    //         </div>
-    //     </div>
-    //     `;
-    // }
+    //variables used to calculate number of rows.
+    const moreColsOnOffsetWidth = 328;
+    const colWidth = 153;
+    const carouselContainer = document.querySelector("#featuredproducts .carousel-inner");
+    //empty current carousel:
+    carouselContainer.innerHTML = "";
+    const cardsInRow = 1 + Math.ceil((carouselContainer.offsetWidth - moreColsOnOffsetWidth) / colWidth);
+    const rowsToBuild = Math.ceil(arr.length / cardsInRow);
+    // Build the carousel "rows":
+    for (let j = 0; j < rowsToBuild; j++) {
+        //console.log("Building row: " + j);
+        carouselContainer.innerHTML += `
+        <div class="featured__container carousel-item carousel-item-row${j}" data-bs-interval="40000">
+            <div class="featured__containerrow row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-2">
+            </div>
+        </div>
+        `;
+    }
     // // Then add each featured product in the correct row.
-    // for (let i = 0; i < arr.length; i++) {
-    //     // Calculates the correct row:
-    //     const rowToGoTo = Math.floor(i / cardsInRow);
-    //     // Use the rownumber to identify the row-element to put it into
-    //     const carouselRowElement = document.querySelector("#featuredproducts .carousel-item-row" + rowToGoTo + " .row");
-    //     carouselRowElement.innerHTML += `
-    //             <div class="col featured__wrapper" data-id="${arr[i].id}">
-    //                 <div class="card h-100 featured__wrapper-card">
-    //                     <div class="card-img featured__wrapper-card-imgbox">
-    //                         <img src="${arr[i].attributes.image_url}" class="card-img-top featured__wrapper-card-imgbox-img" aria-label="Image of ${arr[i].attributes.title}" title="Image of ${arr[i].attributes.title}" alt="Image of ${arr[i].attributes.title}" />
-    //                     </div>
-    //                     <div class="card-body">
-    //                         <h5 class="card-title">${arr[i].attributes.title}</h5>
-    //                         <p class="card-text">${arr[i].attributes.price} EUR</p>
-    //                     </div>
-    //                     <div class="card-footer">Model: ${arr[i].attributes.productnumber}</div>
-    //                 </div>
-    //             </div>
-    //     `;
-    // }
+    for (let i = 0; i < arr.length; i++) {
+        // Calculates the correct row:
+        const rowToGoTo = Math.floor(i / cardsInRow);
+        // Use the rownumber to identify the row-element to put it into
+        const carouselRowElement = document.querySelector("#featuredproducts .carousel-item-row" + rowToGoTo + " .row");
+        carouselRowElement.innerHTML += `
+                <div class="col featured__wrapper" data-id="${arr[i].id}">
+                    <div class="card h-100 featured__wrapper-card">
+                        <div class="card-img featured__wrapper-card-imgbox">
+                            <img src="${arr[i].attributes.image_url}" class="card-img-top featured__wrapper-card-imgbox-img" aria-label="Image of ${arr[i].attributes.title}" title="Image of ${arr[i].attributes.title}" alt="Image of ${arr[i].attributes.title}" />
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">${arr[i].attributes.title}</h5>
+                            <p class="card-text">${arr[i].attributes.price} EUR</p>
+                        </div>
+                        <div class="card-footer">Model: ${arr[i].attributes.productnumber}</div>
+                    </div>
+                </div>
+        `;
+    }
 
     // // set first carousel-item as the active.
-    // document.querySelector("#featuredproducts .carousel-item").classList.add("active");
+    document.querySelector("#featuredproducts .carousel-item").classList.add("active");
 
     // // add eventlisteners:
 
