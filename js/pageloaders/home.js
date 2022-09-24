3a30bcfe93aa498c5b702736e2f83c9c57e766b3
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-24 21:24:19 +0200

    Test with hero

--- js/pageloaders/home.js
+++ js/pageloaders/home.js
@@ -3,7 +3,7 @@ import getHero from "../components/api/getHero.js";
 import getProducts from "../components/api/getProducts.js";
 // import getProducts from "../components/api/getProducts.js";
 
-//getHero();
+getHero();
 getProducts(true).then((products) => {
     console.log(products);
 });
