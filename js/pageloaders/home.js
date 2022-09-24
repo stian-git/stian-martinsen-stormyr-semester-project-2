13c0270d2bd16e117cc84ea296a1140b25a13372
stian-git <82411646+stian-git@users.noreply.github.com>
2022-09-24 21:20:49 +0200

    Another test...

--- js/pageloaders/home.js
+++ js/pageloaders/home.js
@@ -1,9 +1,10 @@
 // import displayFeaturedProducts from "../components/actions/displayFeaturedProducts.js";
 import getHero from "../components/api/getHero.js";
+import getProducts from "../components/api/getProducts.js";
 // import getProducts from "../components/api/getProducts.js";
 
-getHero();
+//getHero();
+getProducts(true);
 // getProducts(true).then((arr) => {
 //     displayFeaturedProducts(arr);
-//     getHero();
 // });
